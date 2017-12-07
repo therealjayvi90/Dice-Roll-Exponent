357cb22e401c2c9703360fc29f12b068ce34aa11
Jay Vi <j.vara90@yahoo.com>
2017-12-06 19:13:55 -0800

    Update dice_multiplier.py
    Added functionality to request user input.

--- dice_multiplier.py
+++ dice_multiplier.py
@@ -1,5 +1,8 @@
+import decimal
+
 def math_func(base, multiplier, rolls):
-    reverse_rolls = 0
+    reverse_rolls = 1
+    print("Roll #1: ", base)
     while rolls > 0:
         increase = base * multiplier
         base = base + increase
@@ -7,4 +10,9 @@ def math_func(base, multiplier, rolls):
         reverse_rolls += 1
         print("Roll #{}: ".format(reverse_rolls), base)
 
-math_func(0.000014, 3.04615, 15)
+
+base = decimal.Decimal(input("Enter the base bet: "))
+multiplier = decimal.Decimal(input("Enter the multiplier: "))
+rolls = int(input("Enter the amount of rolls: "))
+
+math_func(base, multiplier, rolls)
